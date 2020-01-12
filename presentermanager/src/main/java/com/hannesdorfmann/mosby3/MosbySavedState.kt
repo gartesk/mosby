37ddf3b11387ee1b968076e342e1c3af08ff5f32
gartesk <kirill.e.garbuzov@gmail.com>
2020-01-12 21:22:02 +0700

    Updated presentermanager module to kotlin and androidx

---
+++ presentermanager/src/main/java/com/hannesdorfmann/mosby3/MosbySavedState.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2016 Hannes Dorfmann.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package com.hannesdorfmann.mosby3
+
+import android.os.Parcel
+import android.os.Parcelable
+
+import androidx.customview.view.AbsSavedState
+
+/**
+ * The SavedState implementation to store the view's internal id to
+ */
+class MosbySavedState : AbsSavedState {
+
+	companion object {
+
+		@JvmField
+		val CREATOR: Parcelable.Creator<MosbySavedState> =
+			object : Parcelable.ClassLoaderCreator<MosbySavedState> {
+
+				override fun createFromParcel(source: Parcel): MosbySavedState =
+					MosbySavedState(source, MosbySavedState::class.java.classLoader)
+
+				override fun createFromParcel(source: Parcel, loader: ClassLoader?): MosbySavedState {
+					val actualLoader = loader ?: MosbySavedState::class.java.classLoader
+					return MosbySavedState(source, actualLoader)
+				}
+
+				override fun newArray(size: Int): Array<MosbySavedState?> = arrayOfNulls(size)
+			}
+	}
+
+	var mosbyViewId: String? = null
+		private set
+
+	constructor(superState: Parcelable, mosbyViewId: String?) : super(superState) {
+		this.mosbyViewId = mosbyViewId
+	}
+
+	private constructor(source: Parcel, loader: ClassLoader?) : super(source, loader) {
+		this.mosbyViewId = source.readString()
+	}
+
+	override fun writeToParcel(out: Parcel, flags: Int) {
+		super.writeToParcel(out, flags)
+		out.writeString(mosbyViewId)
+	}
+}
