024d36e147b05f3d9500cb2e74201cd8d2f91b3b
gartesk <kirill.e.garbuzov@gmail.com>
2020-01-12 21:22:27 +0700

    Updated mvi module to kotlin and androidx

---
+++ mvi/src/main/java/com/hannesdorfmann/mosby3/ActivityMviDelegateImpl.kt
@@ -0,0 +1,186 @@
+/*
+ * Copyright 2015 Hannes Dorfmann.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hannesdorfmann.mosby3
+
+import android.app.Activity
+import android.os.Bundle
+import android.util.Log
+
+import com.hannesdorfmann.mosby3.mvi.MviPresenter
+import com.hannesdorfmann.mosby3.mvp.MvpPresenter
+import com.hannesdorfmann.mosby3.mvp.MvpView
+import java.util.UUID
+
+/**
+ * The concrete implementation of [ActivityMviDelegate].
+ * This delegate creates the Presenter and attaches the View to the presenter in [Activity.onStart].
+ * The view is detached from presenter in [Activity.onStop]
+ *
+ * @param [V] The type of [MvpView]
+ * @param [P] The type of [MvpPresenter]
+ * @see ActivityMviDelegate
+ */
+class ActivityMviDelegateImpl<V : MvpView, P : MviPresenter<V, *>>
+/**
+ * Creates a new delegate
+ *
+ * @param activity The activity
+ * @param delegateCallback The delegate callback
+ * @param keepPresenterInstance true, if the presenter instance should be kept through screen
+ * orientation changes, false if not (a new presenter instance will be created every time you
+ * rotate your device)
+ */
+constructor(
+	private val activity: Activity,
+	private val delegateCallback: MviDelegateCallback<V, P>,
+	private val keepPresenterInstance: Boolean = true
+) : ActivityMviDelegate<V, P> {
+
+	companion object {
+		var DEBUG = false
+		private const val DEBUG_TAG = "ActivityMviDelegateImpl"
+		private const val KEY_MOSBY_VIEW_ID = "com.hannesdorfmann.mosby3.activity.mvi.id"
+	}
+	private var mosbyViewId: String? = null
+
+	private var presenter: P? = null
+
+	override fun onCreate(bundle: Bundle?) {
+		if (keepPresenterInstance && bundle != null) {
+			mosbyViewId = bundle.getString(KEY_MOSBY_VIEW_ID)
+		}
+
+		if (DEBUG) {
+			Log.d(DEBUG_TAG, "MosbyView ID = $mosbyViewId for MvpView: ${delegateCallback.mvpView}")
+		}
+	}
+
+	override fun onStart() {
+		var viewStateWillBeRestored = false
+
+		val currentMosbyViewId = mosbyViewId
+		if (currentMosbyViewId == null) {
+			// No presenter available,
+			// Activity is starting for the first time (or keepPresenterInstance == false)
+			presenter = createViewIdAndCreatePresenter()
+			if (DEBUG) {
+				Log.d(DEBUG_TAG, "New Presenter instance created: $presenter " +
+						"for ${delegateCallback.mvpView}")
+			}
+		} else {
+			presenter = PresenterManager.getPresenter<P>(activity, currentMosbyViewId)
+			if (presenter == null) {
+				// Process death,
+				// hence no presenter with the given viewState id stored, although we have a viewState id
+				presenter = createViewIdAndCreatePresenter()
+				if (DEBUG) {
+					Log.d(DEBUG_TAG, "No Presenter instance found in cache, although " +
+								"MosbyView ID present. This was caused by process death, therefore " +
+								"new Presenter instance created: $presenter")
+				}
+			} else {
+				viewStateWillBeRestored = true
+				if (DEBUG) {
+					Log.d(DEBUG_TAG, "Presenter instance reused from internal cache: $presenter")
+				}
+			}
+		}
+
+		// presenter is ready, so attach viewState
+		val view = delegateCallback.mvpView
+
+		if (viewStateWillBeRestored) {
+			delegateCallback.setRestoringViewState(true)
+		}
+
+		presenter?.attachView(view)
+
+		if (viewStateWillBeRestored) {
+			delegateCallback.setRestoringViewState(false)
+		}
+
+		if (DEBUG) {
+			Log.d(DEBUG_TAG, "MvpView attached to Presenter. MvpView: $view. Presenter: $presenter")
+		}
+	}
+
+	/**
+	 * Generates the unique (mosby internal) view id and calls [MviDelegateCallback.createPresenter]
+	 * to create a new presenter instance
+	 *
+	 * @return The new created presenter instance
+	 */
+	private fun createViewIdAndCreatePresenter(): P {
+
+		val presenter = delegateCallback.createPresenter()
+		if (keepPresenterInstance) {
+			val generatedMosbyViewId = UUID.randomUUID().toString()
+			mosbyViewId = generatedMosbyViewId
+			PresenterManager.putPresenter(activity, generatedMosbyViewId, presenter)
+		}
+		return presenter
+	}
+
+	override fun onSaveInstanceState(outState: Bundle) {
+		if (keepPresenterInstance) {
+			outState.putString(KEY_MOSBY_VIEW_ID, mosbyViewId)
+			if (DEBUG) {
+				Log.d(DEBUG_TAG, "Saving MosbyViewId into Bundle. ViewId: $mosbyViewId")
+			}
+		}
+	}
+
+	override fun onStop() {
+		presenter?.detachView()
+
+		if (DEBUG) {
+			Log.d(DEBUG_TAG, "Detached MvpView from Presenter. " +
+						"MvpView: ${delegateCallback.mvpView}. Presenter: $presenter")
+		}
+	}
+
+	override fun onDestroy() {
+		presenter?.let { notNullPresenter ->
+			// Presenter is only null if Activity.finish() called before Activity.onStart() has been reached
+			val retainPresenterInstance = activity.shouldRetainPresenterInstance(keepPresenterInstance)
+			if (!retainPresenterInstance) {
+				notNullPresenter.destroy()
+				mosbyViewId?.let {
+					// mosbyViewId == null if keepPresenterInstance == false
+					PresenterManager.remove(activity, it)
+				}
+				Log.d(DEBUG_TAG, "Destroying Presenter permanently $notNullPresenter")
+			}
+
+		}
+		presenter = null
+	}
+
+	override fun onPostCreate(savedInstanceState: Bundle?) {}
+
+	override fun onPause() = Unit
+
+	override fun onResume() = Unit
+
+	override fun onRestart() = Unit
+
+	override fun onContentChanged() = Unit
+
+	override fun onRetainCustomNonConfigurationInstance(): Any? = null
+}
+
+
---
+++ mvi/src/main/java/com/hannesdorfmann/mosby3/mvi/MviDialogFragment.kt
@@ -0,0 +1,176 @@
+/*
+ * Copyright 2016 Hannes Dorfmann.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package com.hannesdorfmann.mosby3.mvi
+
+import android.app.Activity
+import android.content.Context
+import android.content.res.Configuration
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.annotation.CallSuper
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.Fragment
+import com.hannesdorfmann.mosby3.FragmentMviDelegate
+import com.hannesdorfmann.mosby3.FragmentMviDelegateImpl
+import com.hannesdorfmann.mosby3.MviDelegateCallback
+import com.hannesdorfmann.mosby3.mvp.MvpView
+
+/**
+ * This abstract class can be used to extend from to implement an Model-View-Intent pattern with
+ * this dialog fragment as View and a [MviPresenter] to coordinate the View and the underlying
+ * model (business logic)
+ *
+ * Per default [FragmentMviDelegateImpl] is used with the following lifecycle:
+ * The View is attached to the Presenter in [onViewCreated]. So you better instantiate all your
+ * UI widgets before that lifecycle callback (typically in [onCreateView]).
+ *
+ * The View is detached from Presenter in [onDestroyView]
+ */
+abstract class MviDialogFragment<V : MvpView, P : MviPresenter<V, *>> :
+	DialogFragment(), MvpView, MviDelegateCallback<V, P> {
+
+	private var isRestoringViewState = false
+
+	/**
+	 * Get the mvi delegate. This is internally used for creating presenter, attaching and detaching
+	 * viewState from presenter.
+	 *
+	 * **Please note that only one instance of mvi delegate should be used per Fragment instance**.
+	 *
+	 * Only override this property if you really know what you are doing.
+	 *
+	 * @return [FragmentMviDelegate]
+	 */
+	protected val mviDelegate: FragmentMviDelegate<V, P> by lazy {
+		FragmentMviDelegateImpl(this, this)
+	}
+
+	@CallSuper
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		mviDelegate.onCreate(savedInstanceState)
+	}
+
+	@CallSuper
+	override fun onDestroy() {
+		super.onDestroy()
+		mviDelegate.onDestroy()
+	}
+
+	@CallSuper
+	override fun onSaveInstanceState(outState: Bundle) {
+		super.onSaveInstanceState(outState)
+		mviDelegate.onSaveInstanceState(outState)
+	}
+
+	@CallSuper
+	override fun onPause() {
+		super.onPause()
+		mviDelegate.onPause()
+	}
+
+	@CallSuper
+	override fun onResume() {
+		super.onResume()
+		mviDelegate.onResume()
+	}
+
+	@CallSuper
+	override fun onStart() {
+		super.onStart()
+		mviDelegate.onStart()
+	}
+
+	@CallSuper
+	override fun onStop() {
+		super.onStop()
+		mviDelegate.onStop()
+	}
+
+	@CallSuper
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		mviDelegate.onViewCreated(view, savedInstanceState)
+	}
+
+	@CallSuper
+	override fun onDestroyView() {
+		super.onDestroyView()
+		mviDelegate.onDestroyView()
+	}
+
+	@CallSuper
+	override fun onActivityCreated(savedInstanceState: Bundle?) {
+		super.onActivityCreated(savedInstanceState)
+		mviDelegate.onActivityCreated(savedInstanceState)
+	}
+
+	@CallSuper
+	override fun onAttach(activity: Activity) {
+		super.onAttach(activity)
+		mviDelegate.onAttach(activity)
+	}
+
+	override fun onAttach(context: Context) {
+		super.onAttach(context)
+		mviDelegate.onAttach(context)
+	}
+
+	@CallSuper
+	override fun onDetach() {
+		super.onDetach()
+		mviDelegate.onDetach()
+	}
+
+	@CallSuper
+	override fun onAttachFragment(childFragment: Fragment) {
+		super.onAttachFragment(childFragment)
+		mviDelegate.onAttachFragment(childFragment)
+	}
+
+	@CallSuper
+	override fun onConfigurationChanged(newConfig: Configuration) {
+		super.onConfigurationChanged(newConfig)
+		mviDelegate.onConfigurationChanged(newConfig)
+	}
+
+	/**
+	 * Instantiate a presenter instance
+	 *
+	 * @return The [MviPresenter] for this viewState
+	 */
+	abstract override fun createPresenter(): P
+
+	override val mvpView: V
+		get() = try {
+			this as V
+		} catch (e: ClassCastException) {
+			val msg = "Couldn't cast the View to the corresponding View interface. " +
+					"Most likely you forgot to implement your mvi interface in this View."
+			Log.e(this.toString(), msg)
+			throw RuntimeException(msg, e)
+		}
+
+	override fun setRestoringViewState(restoringViewState: Boolean) {
+		isRestoringViewState = restoringViewState
+	}
+
+	protected fun isRestoringViewState(): Boolean {
+		return isRestoringViewState
+	}
+}
