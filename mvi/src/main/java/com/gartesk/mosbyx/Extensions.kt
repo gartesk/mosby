6105906567dae3c9841eb7926da9465c5a53c88e
gartesk <kirill.e.garbuzov@gmail.com>
2020-01-24 22:52:49 +0700

    Updated hack to access fragment backstack

--- mvi/src/main/java/com/gartesk/mosbyx/Extensions.kt
+++ mvi/src/main/java/com/gartesk/mosbyx/Extensions.kt
@@ -17,8 +17,6 @@ package com.gartesk.mosbyx
 
 import android.app.Activity
 import androidx.fragment.app.Fragment
-import java.io.PrintWriter
-import java.io.StringWriter
 
 /**
  * Determines whether or not a Presenter Instance should be kept
@@ -32,16 +30,21 @@ internal fun Activity.shouldRetainPresenterInstance(keepPresenterInstance: Boole
  * Checks whether or not a given fragment is on the backstack of the fragment manager (could also
  * be on top of the backstack and hence visible)
  *
- * As of version 1.0 of AndroidX - fragment.isInBackStack() is package private which leads to
- * an IllegalAccessError being thrown when trying to use it.
- * This method is a temporary workaround until the issue is resolved in AndroidX.
+ * This is a hack since in AndroidX Fragment::isInBackStack is not accessible
  *
  * @return true, if the given Fragment is on the back stack, otherwise false (not on the back
  * stack)
  */
 fun Fragment.isInBackStack(): Boolean {
-	val writer = StringWriter()
-	dump("", null, PrintWriter(writer), null)
-	val dump = writer.toString()
-	return !dump.contains("mBackStackNesting=0")
+	return try {
+		val backStackNestingField = Fragment::class.java
+			.getDeclaredField("mBackStackNesting")
+		backStackNestingField.isAccessible = true
+		val backStackNesting = backStackNestingField.getInt(this)
+		backStackNesting > 0
+	} catch (e: NoSuchFieldException) {
+		throw RuntimeException(e)
+	} catch (e: IllegalAccessException) {
+		throw RuntimeException(e)
+	}
 }
